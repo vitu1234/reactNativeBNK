0e647a62d3dee56bea0c5226acd0acea0b710693
Vitu12345 <vmafeni@agilesystems.pa.mw>
2022-06-29 16:13:12 +0200

    home page with navigation1

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from "./pages/HomeScreen";
 import NotificationsScreen from "./pages/NotificationsScreen";
 import {NativeBaseProvider} from "native-base";
-import { EvilIcons } from '@expo/vector-icons';
+import {EvilIcons} from '@expo/vector-icons';
+import TopUpScreen from "./pages/TopUpScreen";
 
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
     return (
@@ -18,8 +22,16 @@ export default function App() {
                 <Drawer.Navigator initialRouteName="Home">
                     <Drawer.Screen name="Home" component={HomeScreen}/>
                     <Drawer.Screen name="Notifications" component={NotificationsScreen}/>
+                    <Stack.Screen name="TopUpScreen" component={TopUpScreen}/>
+
                 </Drawer.Navigator>
             </NavigationContainer>
+            {/*<NavigationContainer >*/}
+            {/*    <Stack.Navigator>*/}
+            {/*        <Stack.Screen name="TopUpScreen" component={TopUpScreen}/>*/}
+
+            {/*    </Stack.Navigator>*/}
+            {/*</NavigationContainer>*/}
         </NativeBaseProvider>
     );
 }
---
+++ pages/TopUpScreen.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Text, View,
+    TouchableOpacity, PermissionsAndroid } from 'react-native';
+import Ussd, {ussdEventEmitter} from 'react-native-ussd';
+
+
+export default class TopUpScreen extends React.Component {
+    state = {
+        userBalance:0,
+        expiryDate:''
+    };
+
+
+    async checkBalance(){
+        let granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+            {
+                'title': 'I need to make some calls',
+                'message': 'Give me permission to make calls '
+            }
+        )
+
+        if (granted) {
+            console.log( "CAN Make Calls" );
+            Ussd.dial('*#456#');
+
+            console.log(this.state.userBalance);
+        }
+        else {
+            console.log( "CALL MAKING Permission Denied" );
+        }
+    }
+    componentDidMount(){
+
+        this.eventListener = ussdEventEmitter.addListener('ussdEvent', (event) => {
+            console.log(event.ussdReply)
+            let balance = event.ussdReply.split("is")[1].split(".Valid")[0];
+            let date = event.ussdReply.split("until")[1].split(".")[0];
+            this.setState({
+                userBalance:balance,
+                expiryDate:date
+            })
+            console.log(balance);
+        });
+    }
+    componentWillUnmount(){
+        this.eventListener.remove();
+    }
+    render(){
+        return (
+            <View >
+                <TouchableOpacity onPress={() => this.checkBalance()}>
+                    <Text>Check Balance</Text>
+                </TouchableOpacity>
+                <Text>Your Balance is: {this.state.userBalance}</Text>
+                <Text>Expiry Date is: {this.state.expiryDate}</Text>
+
+            </View>
+        );
+    }
+
+}
